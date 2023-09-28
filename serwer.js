238bbddb217cb4c57be892682c495fb9deedeee4
ToMSoN28 <t.kowalski999@gmail.com>
2023-09-28 18:21:29 +0200

    modyfication

--- serwer.js
+++ serwer.js
@@ -3,7 +3,8 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Grupa ZDTESTpol134 jest SUPER SUPER!');
+  //res.end('Grupa ZDTESTpol134 jest SUPER SUPER!');
+  res.end(`<html><body><h1>This is HTML</h1></body></html>`);
 }
 
 const server = http.createServer(requestListener);
